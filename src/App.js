eab7d6e51e06667c87b0b1b2d53ec3d283f236d6
camilorecce <camilorecce@gmail.com>
2020-10-26 18:46:18 -0300

    Added basic functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quoteText: '',
+      quoteAuthor: ''
+    }
+    this.getQuote=this.getQuote.bind(this);
+    this.getNewQuote=this.getNewQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  getNewQuote() {
+    this.getQuote();
+  }
+
+  getQuote() {
+  fetch('https://type.fit/api/quotes')
+    .then(response => response.json())
+    .then(data => {
+      let quotesArr = data;
+      console.log(quotesArr.length);
+      const randomNum = Math.floor(Math.random() * quotesArr.length);
+      console.log(randomNum)
+      this.setState({
+        quoteText: quotesArr[randomNum].text,
+        quoteAuthor: quotesArr[randomNum].author
+      });
+    });
+  }
+
+  render () {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="quote-box">
+      <div id="text">
+        {this.state.quoteText}
+      </div>
+      <div id="author">
+        {this.state.author}
+      </div>
+      <button id="new-quote" onClick={this.getNewQuote}>New Quote</button>
+      <a id="tweet-quote" href="#">
+
+      </a>
     </div>
   );
+}
 }
 
 export default App;
